fffbff93b8c08b1d25f7027534d0451c604ccbb6
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-04 16:34:05 -0300

    First part completed

---
+++ Rascunho/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalInStock()
+        {
+            return Quantity * Price;
+        }
+
+        public void AddProduct(int p)
+        {
+            Quantity += p;
+        }
+
+        public void RemoveProduct(int p)
+        {
+            Quantity -= p;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {Price.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $", {Quantity} units, total: $ {TotalInStock().ToString("F2", CultureInfo.InvariantCulture)}"; 
+        }
+
+    }
+}
---
+++ Rascunho/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            Product product;
+            Console.WriteLine("Enter the product's data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Price: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity: ");
+            int quantity = int.Parse(Console.ReadLine());
+            product = new Product { Name = name, Price = price, Quantity = quantity };
+            Console.WriteLine("Product data: " + product);
+
+            Console.Write("Enter the number of product to be added to the stock: ");
+            product.AddProduct(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Update: " + product);
+
+            Console.Write("Enter the number of product to be removed from the stock: ");
+            product.RemoveProduct(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Update data: " + product);
+
+            Console.ReadKey();
+        }      
+
+    }
+}
